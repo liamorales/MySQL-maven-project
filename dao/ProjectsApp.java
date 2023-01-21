4ea18180ddc5f0527457c241736b0f8349640bc9
liamorales <morales.lia.1990@gmail.com>
2023-01-21 11:00:07 -0600

    first commit

---
+++ dao/ProjectsApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package projects.dao;
+
+/**
+ * @author liamorales
+ *
+ */
+public class ProjectsApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Connection conn = DbConnection.getConnection();
+
+	}
+
+}
